06d3f37b4adb31c11ecbbce2894bcf59c5bd5a96
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-01-29 23:57:18 +0100

    UI-build

---
+++ components/QuizComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+export default function QuizComponent() {
+    return (
+        <>
+            <h3 className="quiz-question">How would one say goodbye in Spanish?</h3>
+            <div className="quiz-options">
+                <div className="options option-1">Adios</div>
+                <div className="options option-2">Hola</div>
+                <div className="options option-3">Au Revoir</div>
+                <div className="options option-4">Salir</div>
+            </div>
+        </>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React, {useState, useEffect} from "react"
 import topRightBlob from "./assets/top-right-blob.png"
 import bottomLeftBlob from "./assets/bottom-left-blob.png"
+import QuizComponent from "../components/QuizComponent"
 
 export default function App() {
 
-  const [isQuizRunning, setIsQuizRunning] = useState(true)
+  //State declaration section
+  const [isQuizRunning, setIsQuizRunning] = useState(false)
+
+  //Function building section
+  function startQuiz() {
+    setIsQuizRunning(prevVal => !prevVal)
+  }
 
   //Everything is still hard coded at this point. this will change in the future
   return isQuizRunning ? (
@@ -12,12 +19,14 @@ export default function App() {
       <img className="bottom-left-blob" src={bottomLeftBlob} alt="Bottom Left Blob"/>
       <img src={topRightBlob} alt="Top Right Blob" className="top-right-blob" />
       <div className="quiz-container">
-        <h3 className="quiz-question">How would one say goodbye in Spanish</h3>
-        <div className="quiz-options">
-          <div className="options option-1">Adios</div>
-          <div className="options option-2">Hola</div>
-          <div className="options option-3">Au Revoir</div>
-          <div className="options option-4">Salir</div>
+        <QuizComponent />
+        <QuizComponent />
+        <QuizComponent />
+        <QuizComponent />
+        <QuizComponent />
+        <QuizComponent />
+        <div className="button-div">
+          <button className="check-answers">Check answers</button>
         </div>
       </div>
     </div>
@@ -29,7 +38,7 @@ export default function App() {
       <section className="quiz-intro">
         <h2 className="title">Quizzical</h2>
         <p className="description">Some description if needed</p>
-        <button className="start-quiz-btn">Start quiz</button>
+        <button className="start-quiz-btn" onClick={startQuiz}>Start quiz</button>
       </section>
     </div>
   ) 
