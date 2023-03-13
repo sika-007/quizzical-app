c17430b3391912cbe44f9c130114aae4609c24f7
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-03-13 19:19:18 +0100

    fonts imported and added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sand": ['Quicksand', "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
