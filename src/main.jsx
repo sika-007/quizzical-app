6a6f696c64559bbaa04aab0b2e6f4a5386b944f7
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-03-13 19:21:20 +0100

    Added context provider above the App component
    This is a change that I may revise in the future

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QuizContextProvider, QuizContext } from './components/QuizContext'
 import "./globals.css"
 import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QuizContextProvider>
+      <App /> 
+    </QuizContextProvider>
   </React.StrictMode>,
 )
